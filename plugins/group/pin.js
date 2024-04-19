49922cd4a14efcd93dc008823c06b4e15b1e9371
xyzuniverse <dev.xyzuniverse@gmail.com>
2024-04-19 21:47:17 +0800

    xyzbot: Add pin feature
    Change-Id: If8ea74a08c36d8df65aca56d3a6dfaa97fed801b
    Signed-off-by: xyzuniverse <dev.xyzuniverse@gmail.com>

---
+++ plugins/group/pin.js
@@ -0,0 +1,29 @@
+let handler = async (msg, { client, text }) => {
+    if (!msg.hasQuotedMsg) {
+        msg.react("⚠");
+        return msg.reply("Reply a message to pin!");
+    }
+    try {
+        var pinTimeout = 86400; // 24 hour
+        var participants = [];
+        for (let users of (await msg.getChat()).groupMetadata.participants) {
+            participants.push(users.id._serialized);
+        }
+        result = (await msg.getQuotedMessage()).pin(pinTimeout);
+        if (result) {
+            msg.react("✅");
+            if (text.includes("loud")) {
+                return msg.reply("I've pinned this message and mentioned everyone members.", msg.from, { mentions: participants })
+            } else return msg.reply("I've pinned this message.")
+        }
+    } catch (e) {
+        console.log(e)
+        return msg.reply("A error occured, please try again later.")
+    }
+}
+
+handler.help = ['pin <time> (default 24hour)'];
+handler.tags = ['group'];
+handler.command = /^(pin)$/i
+
+module.exports = handler;
